81e218f183225468b54d406125e79ef98a78af5b
Hayden Roney <roneyhayden@gmail.com>
2024-03-14 11:20:05 -0500

    updated debugger

--- lib/debug.py
+++ lib/debug.py
@@ -9,14 +9,23 @@ import ipdb
 def reset_database():
     Campaign.drop_table()
     DungeonMaster.drop_table()
+
     Campaign.create_table()
     DungeonMaster.create_table()
 
-    sarah = DungeonMaster.create("Sarah Johnson", "In-person")
-    baron = DungeonMaster.create("Baron Smith", "Online")
 
+    sarah = DungeonMaster.create("Sarah", "In-person")
+    baron = DungeonMaster.create("Baron", "Online")
+    smith = DungeonMaster.create("Smith", "Online")
+    john = DungeonMaster.create("John", "In-person")
     shadow_land = Campaign.create("Shadow Land", baron.id)
     palace_of_the_dead = Campaign.create("Palace of the Dead", sarah.id)
+    space_place = Campaign.create("Space Place", john.id)
+    rat_kingdom = Campaign.create("Rat Kingdom", baron.id)
+    wheel_of_fun = Campaign.create("Wheel of Fun", smith.id)
+
+
+    ipdb.set_trace()
+
 
 reset_database()
-ipdb.set_trace()
